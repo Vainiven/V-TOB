fd9d6abe8765f48dff82a73a5efb7644db58dc99
Vainiven <96945723+Vainiven@users.noreply.github.com>
2022-06-13 17:33:18 +0200

    # WARNING: head commit changed in the meantime
    Merge branch 'main' of https://github.com/Vainiven/V-TOB.git into main

--- src/bosses/PestilentBloat.java
+++ src/bosses/PestilentBloat.java
@@ -10,12 +10,14 @@ import simple.api.coords.WorldArea;
 import simple.api.coords.WorldPoint;
 
 public class PestilentBloat extends TobBoss {
+	
+	WorldArea bossRegion = new WorldArea(new WorldPoint(3288, 4455, 0), new WorldPoint(3303, 4440, 0));
 
 	WorldArea northRegion = new WorldArea(new WorldPoint(3292, 4451, 0), new WorldPoint(3299, 4455, 0));
 	WorldArea eastRegion = new WorldArea(new WorldPoint(3298, 4444, 0), new WorldPoint(3303, 4451, 0));
 	WorldArea westRegion = new WorldArea(new WorldPoint(3288, 4444, 0), new WorldPoint(3292, 4451, 0));
 	WorldArea southRegion = new WorldArea(new WorldPoint(3292, 4444, 0), new WorldPoint(3299, 4440, 0));
-	
+
 	WorldPoint northTile = new WorldPoint(3292, 4450, 0);
 	WorldPoint eastTile = new WorldPoint(3298, 4451, 0);
 	WorldPoint southTile = new WorldPoint(3299, 4445, 0);
@@ -28,7 +30,7 @@ public class PestilentBloat extends TobBoss {
 	@Override
 	public boolean move() {
 		if (ctx.npcs.populate().filter(getBoss()).next().getAnimation() != 8082) {
-		WorldPoint bossLocation = ctx.npcs.populate().filter(getIds()).next().getLocation();
+			WorldPoint bossLocation = ctx.npcs.populate().filter(getIds()).next().getLocation();
 			if (bossLocation.within(northRegion)) {
 				ctx.pathing.step(northTile);
 			} else if (bossLocation.within(eastRegion)) {
@@ -46,13 +48,30 @@ public class PestilentBloat extends TobBoss {
 
 	@Override
 	public boolean getToNextRoom() {
-		// TODO Auto-generated method stub
+		WorldPoint secondBarrier = new WorldPoint(3288, 4447, 0);
+//		
+//		WorldPoint[] path = { new WorldPoint(3190, 4446, 0), new WorldPoint(3191, 4440, 0),
+//				new WorldPoint(3191, 4434, 0), new WorldPoint(3185, 4431, 0), new WorldPoint(3177, 4431, 0),
+//				new WorldPoint(3176, 4424, 0) };
+//
+//		if (ctx.pathing.inArea(bossRegion) && !ctx.objects.populate().filter(32755).isEmpty()) {
+//			ctx.objects.nearest().next().interact(SimpleObjectActions.FIRST);
+//			ctx.onCondition(() -> ctx.dialogue.dialogueOpen());
+//			ctx.keyboard.clickKey(KeyEvent.VK_1);
+//		} else if (!ctx.objects.populate().filter("Formidable passage").isEmpty()) {
+//			ctx.objects.nearest().next().interact(SimpleObjectActions.FIRST);
+//		} else if (!ctx.pathing.inArea(bossRegion)) {
+//			ctx.pathing.walkPath(path);
+//		}
+//		return false;
+//	}
+//		
+//		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean goToBoss() {
-		WorldArea bossRegion = new WorldArea(new WorldPoint(3288, 4455, 0), new WorldPoint(3303, 4440, 0));
 		if (ctx.objects.populate().filter(32755).isEmpty() && !ctx.pathing.inArea(bossRegion)) {
 			ctx.pathing.step(new WorldPoint(3305, 4448, 0));
 		} else if (!ctx.objects.populate().filter(32755).isEmpty() && !ctx.pathing.inArea(bossRegion)) {
